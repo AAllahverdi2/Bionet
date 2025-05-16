6a6e21be754c0be6ad82d9df6a52cbb9f85b8b18
AAllahverdi2 <agamaliyevallahverdi2@gmail.com>
2025-05-16 15:01:32 +0400

    header fix

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,132 +1,186 @@
-import { useState, useEffect } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import logo from '../assets/image/logo.png';
-import '../assets/css/style.css';
+import { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import logo from "../assets/image/logo.png";
+import "../assets/css/style.css";
 declare const bootstrap: any;
 const Header = () => {
-    const [searchVisible, setSearchVisible] = useState(false);
-    const location = useLocation();
-    const [, setIsOffcanvasVisible] = useState(false);
-    // const toggleSearch = () => {
-    //     setSearchVisible(!searchVisible);
-    // };
+  const location = useLocation();
+  const [, setIsOffcanvasVisible] = useState(false);
 
-    const closeOffcanvas = () => {
-        const offcanvasElement = document.getElementById('offcanvasLeft');
-        if (offcanvasElement) {
-            const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
-            if (offcanvas) {
-                offcanvas.hide();
-            } else {
-                new bootstrap.Offcanvas(offcanvasElement).hide();
-            }
-        }
-    };
+  const closeOffcanvas = () => {
+    const offcanvasElement = document.getElementById("offcanvasLeft");
+    if (offcanvasElement) {
+      const offcanvas = bootstrap.Offcanvas.getInstance(offcanvasElement);
+      if (offcanvas) {
+        offcanvas.hide();
+      } else {
+        new bootstrap.Offcanvas(offcanvasElement).hide();
+      }
+    }
+  };
 
-    useEffect(() => {
-        closeOffcanvas();
-    }, [location]);
+  useEffect(() => {
+    closeOffcanvas();
+  }, [location]);
 
-    useEffect(() => {
-        const offcanvasElement = document.getElementById('offcanvasLeft');
-        if (offcanvasElement) {
-            const handleShow = () => setIsOffcanvasVisible(true);
-            const handleHide = () => setIsOffcanvasVisible(false);
+  useEffect(() => {
+    const offcanvasElement = document.getElementById("offcanvasLeft");
+    if (offcanvasElement) {
+      const handleShow = () => setIsOffcanvasVisible(true);
+      const handleHide = () => setIsOffcanvasVisible(false);
 
-            offcanvasElement.addEventListener('show.bs.offcanvas', handleShow);
-            offcanvasElement.addEventListener('hidden.bs.offcanvas', handleHide);
+      offcanvasElement.addEventListener("show.bs.offcanvas", handleShow);
+      offcanvasElement.addEventListener("hidden.bs.offcanvas", handleHide);
 
-            return () => {
-                offcanvasElement.removeEventListener('show.bs.offcanvas', handleShow);
-                offcanvasElement.removeEventListener('hidden.bs.offcanvas', handleHide);
-            };
-        }
-    }, []);
+      return () => {
+        offcanvasElement.removeEventListener("show.bs.offcanvas", handleShow);
+        offcanvasElement.removeEventListener("hidden.bs.offcanvas", handleHide);
+      };
+    }
+  }, []);
 
-    return (
-        <header className="header container">
-            <div className="header__left">
-                <Link to="/">
-                    <img src={logo} alt="Bionet Logo" className="header__logo" />
-                </Link>
-            </div>
-            <div className="header__right">
-                <nav className="header__nav">
-                    <Link to="/projects" className={location.pathname === '/projects' ? 'active' : ''}>
-                        Layihələrimiz
-                    </Link>
-                    <Link to="/services" className={location.pathname === '/services' ? 'active' : ''}>
-                        Xidmətlərimiz
-                    </Link>
-                    <Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>
-                        Haqqımızda
-                    </Link>
-                    <Link to="/contact" className={location.pathname === '/contact' ? 'active' : ''}>
-                        Əlaqə
-                    </Link>
-                </nav>
+  return (
+    <header className="header container">
+      <div className="header__left">
+        <Link to="/">
+          <img src={logo} alt="Bionet Logo" className="header__logo" />
+        </Link>
+      </div>
+      <div className="header__right">
+        <nav className="header__nav">
+          <Link
+            to="/projects"
+            className={location.pathname === "/projects" ? "active" : ""}
+          >
+            Layihələrimiz
+          </Link>
+          <Link
+            to="/services"
+            className={location.pathname === "/services" ? "active" : ""}
+          >
+            Xidmətlərimiz
+          </Link>
+          <Link
+            to="/about"
+            className={location.pathname === "/about" ? "active" : ""}
+          >
+            Haqqımızda
+          </Link>
+          <Link
+            to="/contact"
+            className={location.pathname === "/contact" ? "active" : ""}
+          >
+            Əlaqə
+          </Link>
+        </nav>
 
-                <div className="header__search-container">
-                    <i className={`fa-solid fa-magnifying-glass header__search-icon ${searchVisible ? '' : 'hidden'}`}></i>
-                    {/* <input
-                        type="text"
-                        className={`header__search-input ${searchVisible ? 'visible' : ''}`}
-                        placeholder="Search..."
-                    /> */}
-                    <button className="header__search-button" >
+        {/* <div className="header__search-container"> */}
+        {/* <i className="fa-solid fa-magnifying-glass header__search-icon"></i> */}
+        {/* <input
+            type="text"
+            className={`header__search-input ${searchVisible ? "visible" : ""}`}
+            placeholder="Search..."
+          /> */}
+        {/* <button className="header__search-button" >
                         <i className={`fa-solid ${searchVisible ? 'fa-times' : 'fa-magnifying-glass'}`}></i>
-                    </button>
-                </div>
+                    </button> */}
+        {/* </div> */}
+        <span className="header__lang">En</span>
 
-                <span className="header__lang">En</span>
-                <Link to='/contact' className="header__contact">BİZƏ YAZIN</Link>
+        <Link to="/contact" className="header__contact">
+          BİZƏ YAZIN
+        </Link>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <span className="header__lang header__lang_en">En</span>
+        <div className="offcanvasForResponse">
+          <button
+            className="btn btn-primary offbtn"
+            type="button"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasLeft"
+            aria-controls="offcanvasLeft"
+          >
+            <i className="fa-solid fa-bars"></i>
+          </button>
+
+          <div
+            className="offcanvas offcanvas-start"
+            tabIndex={-1}
+            id="offcanvasLeft"
+            aria-labelledby="offcanvasLeftLabel"
+          >
+            <div className="offcanvas-header">
+              <h5 id="offcanvasLeftLabel"></h5>
+              <button
+                type="button"
+                className="btn-close text-reset"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+                onClick={closeOffcanvas}
+              ></button>
             </div>
-
-            <div className="offcanvasForResponse">
-                <button
-                    className="btn btn-primary offbtn"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasLeft"
-                    aria-controls="offcanvasLeft"
-                >
-                    <i className="fa-solid fa-bars"></i>
-                </button>
-
-                <div className="offcanvas offcanvas-start" tabIndex={-1} id="offcanvasLeft" aria-labelledby="offcanvasLeftLabel">
-                    <div className="offcanvas-header">
-                        <h5 id="offcanvasLeftLabel"></h5>
-                        <button
-                            type="button"
-                            className="btn-close text-reset"
-                            data-bs-dismiss="offcanvas"
-                            aria-label="Close"
-                            onClick={closeOffcanvas}
-                        ></button>
-                    </div>
-                    <div className="offcanvas-body">
-                        <Link to="/projects" onClick={closeOffcanvas} className={location.pathname === '/projects' ? 'activeForOffcanvas' : ''}>
-                            Layihələrimiz
-                        </Link>
-                        <hr />
-                        <Link to="/services" onClick={closeOffcanvas} className={location.pathname === '/services' ? 'activeForOffcanvas' : ''}>
-                            Xidmətlərimiz
-                        </Link>
-                        <hr />
-                        <Link to="/about" onClick={closeOffcanvas} className={location.pathname === '/about' ? 'activeForOffcanvas' : ''}>
-                            Haqqımızda
-                        </Link>
-                        <hr />
-                        <Link to="/contact" onClick={closeOffcanvas} className={location.pathname === '/contact' ? 'activeForOffcanvas' : ''}>
-                            Əlaqə
-                        </Link>
-                        <hr />
-                        <Link to='/contact' className="header__contact btn-response" onClick={closeOffcanvas}>BİZƏ YAZIN</Link>
-                    </div>
-                </div>
+            <div className="offcanvas-body">
+              <Link
+                to="/projects"
+                onClick={closeOffcanvas}
+                className={
+                  location.pathname === "/projects" ? "activeForOffcanvas" : ""
+                }
+              >
+                Layihələrimiz
+              </Link>
+              <hr />
+              <Link
+                to="/services"
+                onClick={closeOffcanvas}
+                className={
+                  location.pathname === "/services" ? "activeForOffcanvas" : ""
+                }
+              >
+                Xidmətlərimiz
+              </Link>
+              <hr />
+              <Link
+                to="/about"
+                onClick={closeOffcanvas}
+                className={
+                  location.pathname === "/about" ? "activeForOffcanvas" : ""
+                }
+              >
+                Haqqımızda
+              </Link>
+              <hr />
+              <Link
+                to="/contact"
+                onClick={closeOffcanvas}
+                className={
+                  location.pathname === "/contact" ? "activeForOffcanvas" : ""
+                }
+              >
+                Əlaqə
+              </Link>
+              <hr />
+              <Link
+                to="/contact"
+                className="header__contact btn-response"
+                onClick={closeOffcanvas}
+              >
+                BİZƏ YAZIN
+              </Link>
             </div>
-        </header>
-    );
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
